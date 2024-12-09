10c6ea52545aa36cdc41f63fa8bc04b985120a0b
Oleksii Herashchenko <alex.h@bilberrry.com>
2024-12-09 22:36:17 +0200

    Day 9 optimize

--- 009/009.2.js
+++ 009/009.2.js
@@ -1,7 +1,8 @@
 import {processLineByLine} from "../utils.js";
 
 let input;
-const empty = '.';
+
+const empty = null;
 
 await processLineByLine('data.txt', (line) => {
   input = line.split('').map(v => +v);
@@ -11,7 +12,7 @@ console.time('EXECUTION')
 let map = [];
 for (let i = 0; i < input.length; i++) {
   for (let j = 0; j < input[i]; j++) {
-    map.push(i % 2 === 0 ? Math.floor(i / 2) : empty);
+    map.push(i % 2 === 0 ? i / 2 : empty);
   }
 }
 
@@ -19,14 +20,17 @@ for (let i = input.length - 1; i >= 0; i--) {
   if (i % 2 === 0) {
     const id = i / 2;
     const len = input[i];
-    const pos = map.indexOf(id)
+    let pos = 0;
+    for (let j = 0; j < i; j++) {
+      pos = pos+input[j]
+    }
     let emptyLen = 0;
     for (let j = 0; j < pos; j++) {
-      if(map[j] === '.') {
+      if(map[j] === empty) {
         emptyLen++;
         if(emptyLen === len) {
           for (let k = 0; k < len; k++) {
-            map[pos+k] = '.';
+            map[pos+k] = empty;
             map[j-emptyLen+k+1] = id;
           }
           break;
